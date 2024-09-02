0153e6313418bb72a5eb0ba9fb62147be1b4f4cd
Tochinet <tochinet@gmail.com>
2024-09-02 22:38:27 +0200

    Update PositTest.ino

--- examples/PositTest/PositTest.ino
+++ examples/PositTest/PositTest.ino
@@ -1,7 +1,7 @@
 #include "Posit.h"
 
 char cs[80]; // C string for table
-double numbersList[16]= {0,NAN,1.0,2.0,3.0,4.0,7.0,10.0,15.0,25.0,30.0,0.5,0.1,0.02,0.005,0.0001};
+double numbersList[16]= {0,NAN,1.0,2.0,3.0,4.0,7.0,10.0,15.0,25.0,33.0,0.5,0.12,0.05,0.005,0.0001};
 
 void setup() {
   Serial.begin(9600);
@@ -15,65 +15,86 @@ void setup() {
   } 
   Serial.println(); //*/
 
-  Serial.println("Creation of two posit8 values from double"); 
-  Posit8 firstPosit (-0.19);
-  Posit8 secondPosit (0.65);
-  Serial.print("First: ");
+  Serial.println("Creation of two posit8 values from input strings"); 
+  Serial.println("First Posit? ");
+  while (Serial.available() == 0) {
+   }
+  Posit8 firstPosit (Serial.parseFloat());
+  //delay(300);
+    while (Serial.available() > 0) { // Eliminate extra chars
+      Serial.read();
+    }
+  Serial.println("Second Posit? ");
+  while (Serial.available() == 0) { 
+  }
+  Posit8 secondPosit (Serial.parseFloat());
+  
+  Serial.print("First(");
   Serial.print(firstPosit.value, BIN);
-  Serial.print(" ");
+  Serial.print(") ");
   Serial.println(posit2float(firstPosit));
-  Serial.print("Second: ");
+  Serial.print("Second(");
   Serial.print(secondPosit.value, BIN);
-  Serial.print(" ");
+  Serial.print(") ");
   Serial.println(posit2float(secondPosit)); //*/
 
   Serial.println("Test of operations");  
   Posit8 sum = Posit8::posit8_add(firstPosit, secondPosit);
   //Posit8 sum = firstPosit + secondPosit;  // doesn"t work yet
-  Serial.print("Sum : ");
+  Serial.print("Sum(");
   Serial.print(sum.value, BIN);
-  Serial.print(" ");
+  Serial.print(") ");
   Serial.println(posit2float(sum));
 
 	Posit8 sub = Posit8::posit8_sub(firstPosit, secondPosit);  
   //Posit8 sub = firstPosit - secondPosit;  // doesn"t work yet
-  Serial.print("Sub : ");
+  Serial.print("Sub(");
   Serial.print(sub.value, BIN);
-  Serial.print(" ");
+  Serial.print(") ");
   Serial.println(posit2float(sub)); //*/	
   
   Posit8 mul = posit8_mul(firstPosit, secondPosit);  
   //Posit8 mul = firstPosit * secondPosit;  // doesn"t work yet
-  Serial.print("Mul : ");
+  Serial.print("Mul(");
   Serial.print(mul.value, BIN);
-  Serial.print(" ");
+  Serial.print(") ");
   Serial.println(posit2float(mul)); //*/	
   
   Posit8 div = posit8_div(firstPosit, secondPosit);  
   //Posit8 div = firstPosit / secondPosit;  // doesn"t work yet
-  Serial.print("Div : ");
+  Serial.print("Div(");
   Serial.print(div.value, BIN);
-  Serial.print(" ");
+  Serial.print(") ");
   Serial.println(posit2float(div)); //*/
 
+  randomSeed(millis());
   char as[10],bs[10];
-  Serial.println("Posit Creation from double"); 
+  Serial.println("Table of random results"); 
   Serial.println("   A   :   B   :   abin  :   bbin  :   sum   :   sub   :   mul   :   div");
   Serial.println("-------+-------+---------+---------+---------+---------+---------+---------");
-  /*/for (int i=0; i<16; i++)
-    for (int j=0; j<16; j++) {
+  for (int j=0; j<16; j++) {
       firstPosit=Posit8(numbersList[random(16)]); 
       secondPosit=Posit8(numbersList[random(16)]); 
-      itoa(firstPosit.value,as,2);
-      itoa(secondPosit.value,bs,2);
       sum = Posit8::posit8_add(firstPosit, secondPosit);
     	sub = Posit8::posit8_sub(firstPosit, secondPosit);  
       mul = posit8_mul(firstPosit, secondPosit);  
       div = posit8_div(firstPosit, secondPosit); 
 
-      sprintf(cs, "%07.3f+%07.3f+%8s+%8s+%07.f+%07.f+%07.f+%07.f", posit2float(firstPosit), 
-        posit2float(secondPosit),*as,*bs,posit2float(sum),posit2float(sub),posit2float(mul),posit2float(div));
-      Serial.println(cs);
+      Serial.print(posit2float(firstPosit),3); // No way to align numbers in table
+      Serial.print(" + ");
+      Serial.print(posit2float(secondPosit),3);
+      Serial.print(" + ");
+      Serial.print(firstPosit.value,BIN);
+      Serial.print(" + ");
+      Serial.print(secondPosit.value,BIN);
+      Serial.print(" + ");
+      Serial.print(posit2float(sum),3);
+      Serial.print(" + ");
+      Serial.print(posit2float(sub),3);
+      Serial.print(" + ");
+      Serial.print(posit2float(mul),3);
+      Serial.print(" + ");
+      Serial.println(posit2float(div),3);
     } //*/
 }
 
